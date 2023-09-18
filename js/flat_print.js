3e208e5cd51334e0220e8fa1d6cacc432e584f3a
Asrumochan <asrumochanparida99@gmail.com>
2023-09-18 15:57:16 +0530

    sdf

--- js/flat_print.js
+++ js/flat_print.js
@@ -1,3 +1,12 @@
 const flat_print=function (arr) {
-    
+    return arr.flat(Infinity);
 }
+arr=[['Hello'], [[2]], [[[['everyone']]]]];
+arr1=flat_print(arr);
+console.log(arr1);
+var str=" ";
+arr1.forEach(element => {
+    str=str.concat(" ",element)
+});
+
+console.log(str);
